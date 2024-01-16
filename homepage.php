492e81df2d7d4f22909e2f2727d8c431fbbb1246
Arvhie abarquez <Arvhieabarquez@gmail.com>
2024-01-16 21:52:49 +0800

    Save all

---
+++ homepage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="homepage">
+      <div class="div">
+        <img class="cart1" src="img/cart1.jpg" />
+        <img class="profile1" src="img/profile1.jpg" />
+        <img class="home1" src="img/home1.jpg" />
+        <img class="notifi1" src="img/notifi1.jpg" />
+        <div class="overlap"><div class="text-wrapper">DRINK’S</div></div>
+        <div class="overlap-group"><div class="text-wrapper-2">SNACK’S</div></div>
+        <div class="div-wrapper"><div class="text-wrapper-3">Viand</div></div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login form</title>
+    
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="form-container">
+
+    <form action="" method="post">
+        <img type="logo1" src="img/logo1.jpg" />
+        <input type="email" name="email" required placeholder="Enter your email">
+        <input type="password" name="password" required placeholder="Enter your password">
+        <input type="submit" name="submit" value="login now" class="form-btn">
+        <p>Don't have an account? <a href="register_form.php">Register now</a></p>
+    </form>
+    
+</body>
+</html>
