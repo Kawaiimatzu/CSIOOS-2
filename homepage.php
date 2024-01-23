a06519a8ea31a9e693148a6b4e5123ba0bbeef41
Arvhie abarquez <Arvhieabarquez@gmail.com>
2024-01-24 05:48:19 +0800

    new

--- homepage.php
+++ homepage.php
@@ -12,9 +12,6 @@
         <img class="profile1" src="img/profile1.jpg" />
         <img class="home1" src="img/home1.jpg" />
         <img class="notifi1" src="img/notifi1.jpg" />
-        <div class="overlap"><div class="text-wrapper">DRINK’S</div></div>
-        <div class="overlap-group"><div class="text-wrapper-2">SNACK’S</div></div>
-        <div class="div-wrapper"><div class="text-wrapper-3">Viand</div></div>
       </div>
     </div>
   </body>
